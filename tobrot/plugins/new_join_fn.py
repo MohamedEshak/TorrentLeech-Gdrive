f6832c8825e5142bea01d0f5f05842490ca840cb
MohamedEshak <67477489+MohamedEshak@users.noreply.github.com>
2020-08-23 01:07:13 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text(Follow Soona_Paana)
+    await message.reply_text(Follow Soona Paana)
 
 
 async def rename_message_f(client, message):
